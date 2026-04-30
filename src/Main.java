535b6c91e11a1388b620924ae2073470cab34eca
Salavat2026 <Salavat92@bk.ru>
2026-05-01 01:36:21 +0500

    Сделал ДЗ..

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,24 @@ public class Main {
         Product N3 = new Product(0003, "book", 200, "книги");
         System.out.println(N3);
 
+
         System.out.println(N1.equals(N2)); // true т.к. id и категории совпали
         System.out.println(N1.equals(N3)); // false т.к. id b категории не совпали
 
+        Product[] basket1 = {N1, N3};
+        Product[] basket2 = {new Product(001, "ручка", 15, "концтовары"), N3};
+        System.out.println(Arrays.toString(basket1));
+        System.out.println(Arrays.toString(basket2));
+
+        Order order1 = new Order("Лиля", basket1);
+        Order order2 = new Order("Лиля", basket2);
+        Order order3 = new Order("Роман", basket1);
+        System.out.println(order1);
+        System.out.println(order2);
+        System.out.println(order3);
+
+        System.out.println("Заказ 1 == Заказ 2: " + order1.equals(order2)); // true
+        System.out.println("Заказ 1 == Заказ 3: " + order1.equals(order3)); // false
 
     }
 }
